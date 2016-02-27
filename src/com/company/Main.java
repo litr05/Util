5d738bf06abe3bd443f6501a6cc2fab5ee59e33e
litr05 <litrovkin@yandex.ru>
2016-02-28 00:55:20 +0300

    my

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,11 +9,11 @@ public class Main {
 
         /*System.out.println(Ping.pingReacheble("localhost", 3000));*/
 
-/*      IPv4 ipv4 = new IPv4("192.168.20.45/30");
+      IPv4 ipv4 = new IPv4("192.168.20.45/30");
         System.out.println(ipv4.getIP());
         System.out.println(ipv4.getNetmask());
         System.out.println(ipv4.getCIDR());
-        System.out.println(ipv4.getHostAddressRange());*/
+        System.out.println(ipv4.getHostAddressRange());
 
         MD5Util mesd = new MD5Util();
         String st = mesd.md5Custom(mesd.md5Custom("625810"));
